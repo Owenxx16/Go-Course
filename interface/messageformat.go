9e05f5dd7493d41e98991e1f8bf9f237a753dd9e
Owen <150342381+Owenxx16@users.noreply.github.com>
2025-07-04 20:38:18 +0700

    Day 3

---
+++ interface/messageformat.go
@@ -0,0 +1,35 @@
+package main
+
+// Don't Touch below this line
+
+type Formatter interface {
+	Format() string
+}
+
+type PlainText struct {
+	message string
+}
+
+type Bold struct {
+	message string
+}
+
+type Code struct {
+	message string
+}
+
+func (p PlainText) Format() string {
+	return p.message
+}
+
+func (b Bold) Format() string {
+	return "**" + b.message + "**"
+}
+
+func (c Code) Format() string {
+	return "`" + c.message + "`"
+}
+
+func SendMessage(formatter Formatter) string {
+	return formatter.Format() // Adjusted to call Format without an argument
+}
